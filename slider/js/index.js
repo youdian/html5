1f078748cc2362eb3b59e2364b394be30f2e5d99
shidong <shidong@yiran.com>
2016-05-19 18:46:28 +0800

    resize窗口时自动移动slider位置

--- slider/js/index.js
+++ slider/js/index.js
@@ -2,7 +2,7 @@ function init() {
     var slider = document.getElementById("slider");
     var items = document.getElementsByTagName("img");
     var size = items.length;
-    var sliding = setInterval(switcher, 3000);
+    var sliding = setInterval(switcher, 5000);
     var now=0;
     function switcher () {
         if (now == size - 1) {
@@ -10,11 +10,23 @@ function init() {
         } else{
             now++;
         }
-        var width = items[0].width;//缩放浏览器时宽度会变化，所以写在这里
+        var width = items[0].width;//缩放浏览器时宽度会变化，所以要写在这里
         console.log("now=" + now);
         slider.style.left = -width*now + "px";
         console.log("slider left=" + slider.style.left);
     };
+    function resize() {
+        slider.classList.remove("scroll");
+        var width = items[0].width;//缩放浏览器时宽度会变化，所以要写在这里
+        console.log("now=" + now);
+        slider.style.left = -width*now + "px";
+        slider.classList.add("scroll");
+    }
+    window.onresize = resize;
 };
 
 window.onload= init;
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+  });
