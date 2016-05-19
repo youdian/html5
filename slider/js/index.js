d28e09fc5d62c02e6870f353163f1cb9480eff80
shidong <shidong@yiran.com>
2016-05-19 17:16:05 +0800

    slider自适应布局

--- slider/js/index.js
+++ slider/js/index.js
@@ -1,17 +1,20 @@
 function init() {
     var slider = document.getElementById("slider");
-    var now=0;
     var items = document.getElementsByTagName("img");
     var size = items.length;
-    setInterval(function() {
+    var sliding = setInterval(switcher, 3000);
+    var now=0;
+    function switcher () {
         if (now == size - 1) {
             now=0;
         } else{
             now++;
         }
+        var width = items[0].width;//缩放浏览器时宽度会变化，所以写在这里
         console.log("now=" + now);
-        slider.style.left = -100*now + "px";
+        slider.style.left = -width*now + "px";
         console.log("slider left=" + slider.style.left);
-    }, 3000);
+    };
 };
+
 window.onload= init;
